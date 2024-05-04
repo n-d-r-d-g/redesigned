7a45ad57e634ec4289dc79521368c5a08718b197
n-d-r-d-g <n.d.r.d.g@hotmail.com>
2024-05-04 14:31:48 +0400

    [main] Fix attempt for production server error

--- mauritius_tax_calculator/next.config.js
+++ mauritius_tax_calculator/next.config.js
@@ -4,6 +4,11 @@ const { i18n } = require("./next-i18next.config");
 const nextConfig = {
   reactStrictMode: true,
   i18n,
+  experimental: {
+    // this includes files from the monorepo base one directory up
+    // https://nextjs.org/docs/pages/api-reference/next-config-js/output#caveats
+    outputFileTracingRoot: path.join(__dirname, "../"),
+  },
 };
 
 module.exports = nextConfig;
