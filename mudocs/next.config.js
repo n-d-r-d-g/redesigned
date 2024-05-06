9e649a4d62b9ee77db07a1d20db8a6e439207c6a
n-d-r-d-g <n.d.r.d.g@hotmail.com>
2024-05-06 14:00:08 +0400

    [main][mudocs] Added next-config experimental config to fix nextjs runtime error: `Cannot find module`

--- mudocs/next.config.js
+++ mudocs/next.config.js
@@ -1,10 +1,16 @@
 const { i18n } = require("./next-i18next.config");
+const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   i18n,
-  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
+  pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
+  experimental: {
+    // this includes files from the monorepo base one directory up
+    // https://nextjs.org/docs/pages/api-reference/next-config-js/output#caveats
+    outputFileTracingRoot: path.join(__dirname, "../"),
+  },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
