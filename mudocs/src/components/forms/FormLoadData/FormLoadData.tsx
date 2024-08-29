b33e43a31988c2b72d02fce30d8fa01ff9cc6726
n-d-r-d-g <n.d.r.d.g@hotmail.com>
2024-08-29 19:24:49 +0400

    Added FormLoadData

---
+++ mudocs/src/components/forms/FormLoadData/FormLoadData.tsx
@@ -0,0 +1,32 @@
+import { Button } from "@nextui-org/react";
+import { useFormikContext } from "formik";
+import { useTranslation } from "next-i18next";
+import { useCallback } from "react";
+import { DEFAULT_I18N_NAMESPACE } from "../../../../constants";
+
+type FormLoadDataProps = {
+  data: unknown;
+};
+
+export default function FormLoadData({ data }: FormLoadDataProps) {
+  const { t: tCommon } = useTranslation(DEFAULT_I18N_NAMESPACE);
+  const { isSubmitting, setValues } = useFormikContext();
+  const isDisabled = isSubmitting;
+
+  const handleLoadDummyData = useCallback(() => {
+    setValues(data);
+  }, [data, setValues]);
+
+  return (
+    <Button
+      variant="flat"
+      size="md"
+      radius="sm"
+      className="grow bg-gray-200/70 xs:grow-0 dark:bg-slate-800"
+      isDisabled={isDisabled}
+      onPress={handleLoadDummyData}
+    >
+      {tCommon("loadDummyData")}
+    </Button>
+  );
+}
